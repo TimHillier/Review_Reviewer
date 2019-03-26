ce62752025ef8e1c28e10063f0cb6b60114643eb
Tim Hillier <Tim.r.hillier@gmail.com>
2019-03-26 13:41:00 -0700

    Im getting 99% accuracy but idk if it actually works.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # CPSC Assignment 3
+
+# Install requirements
+```
+pip install -r requirements.txt
+```
+# Run the code
+To run the code simply 
+```
+python main.py
+```
+Theres no arguments to feed it or data to input. All the data is self contained in the provided data packages. 
+
+# The Report Part
+The first thing to do is to read the data into the program. That is what "getData" Does. It takes the training and 
+testing data and converts each review into a utf-8 list. It also Takes the 1,0 for positive and negitive reviews
+and turns them into a more readable version. 
+Once we Have the data we need to make it readable. We do this by finding the unique words of each review and marking 
+those as one. So in the end we have a large matrix of unique words and marking for where each word belongs to which review. 
+After trying a couple different algorithms, for this assignment I decided to use logical regression as it was the 
+easiest to implement. Using the logistical regression in the sklearn python package, the major parameter that I needed to 
+finetune was the regulator.This was easy to do as we can just run the testing data against itself using different values for 
+C and we can find the best one. In our case the best value was C=.25 Using this value We can continue with the assignment.
+After training the classifier on the test data and testing it on the testing data we end up with a high >90% 
+classification effectiveness.
--- analyze.py
+++ analyze.py
@@ -15,14 +15,6 @@ This just gie positive for everything its dumb
 Train the data using the training input
 returns an array [x_train,x_test,y_train,y_test]
 '''
-# def train(data,labels):
-#     vectorizer = CountVectorizer(analyzer='word',lowercase=False)
-#     features = vectorizer.fit_transform(data)
-#     features_array = features.toarray()
-#     X_train,X_test,Y_train,Y_test = train_test_split(features_array,labels,train_size=.8,random_state=1234)
-#     features = [X_train,Y_train,X_test,features_array,Y_test]
-#     return features
-
 def train(data,final_data,labels):
     # print("L",labels)
     countVec = CountVectorizer(binary=True)
@@ -31,9 +23,13 @@ def train(data,final_data,labels):
     X_test = countVec.transform(final_data)
     X_train,X_val,y_train,y_val = train_test_split(X,labels,train_size=0.75)
     features = [X_train,y_train,y_val,X_val,X_test,X] #This might need to be checked
-    return  features
+    return features
 
 
+'''
+predict the data, based on the training.
+might have to pass it the model? (the train/test ^^^
+'''
 def predict(data,labels,model):
     # print("L",labels)
     X_train = model[0]
@@ -42,49 +38,23 @@ def predict(data,labels,model):
     X_val   = model[3]
     X_test  = model[4]
 
-    #for testing different values
-    for c in [0.01,0.05,0.25,0.5,1]:
-        lr = LogisticRegression(C=c)
-        lr.fit(X_train,y_train)
-        print("accuracy for C=%s: %s" %(c,accuracy_score(y_val,lr.predict(X_val))))
-
-
-
-        # accuracy for C=0.01: 0.7745803357314148
-        # accuracy for C=0.05: 0.7937649880095923
-        # accuracy for C=0.25: 0.8081534772182254
-        # accuracy for C=0.5:  0.7985611510791367
-        # accuracy for C=1:    0.8009592326139089
-        # best accuracy is c = 0.25
+    # #for testing different values of c
+    # for c in [0.01,0.05,0.25,0.5,1]:
+    #     lr = LogisticRegression(C=c)
+    #     lr.fit(X_train,y_train)
+    #     print("accuracy for C=%s: %s" %(c,accuracy_score(y_val,lr.predict(X_val))))
+    #
+    #
+    #
+    #     # accuracy for C=0.01: 0.7745803357314148
+    #     # accuracy for C=0.05: 0.7937649880095923
+    #     # accuracy for C=0.25: 0.8081534772182254
+    #     # accuracy for C=0.5:  0.7985611510791367
+    #     # accuracy for C=1:    0.8009592326139089
+    #     # best accuracy is c = 0.25
 
     final_model = LogisticRegression(C=0.25)
     final_model.fit(X_test,labels)
     print("Final Acuracy: %s"%accuracy_score(labels,final_model.predict(X_test)))
 
-'''
-predict the data, based on the training.
-might have to pass it the model? (the train/test ^^^
-'''
-# def predict(data,model):
-#     # print(data)
-#     X_train = model[0]
-#     Y_train = model[1]
-#     X_test = model [2]
-#     feature_array = model[3]
-#     y_test = model[4]
-#
-#     print(X_test)
-#     log_model = LogisticRegression()
-#     log_model = log_model.fit(X=X_train,y=Y_train)
-#     y_predict = log_model.predict(X_test)
-#     # y_predict = log_model.predict(data)
-#     j = random.randint(0, len(X_test) - 7)
-#     # for i in range(j, j + 7):
-#     for i in range(0,3):
-#         # print(len(y_predict))
-#         print(y_predict[0])
-#         ind = feature_array.tolist().index(X_test[i].tolist())
-#         # print("ind",ind)
-#         print(data[ind].strip())
-#     # print(accuracy_score(y_test,y_predict))
 
